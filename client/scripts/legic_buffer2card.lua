e726fd75d3b21b6b22c646204ae8ccf11bf6aa29
mosci <icsom@gmx.de>
2016-02-23 07:45:57 +0100

    added icemans implementation of Crc8Legic in utils.lua and modification to legic_clone.lua

--- client/scripts/legic_buffer2card.lua
+++ client/scripts/legic_buffer2card.lua
@@ -6,34 +6,15 @@ function sleep(n)  -- seconds
    end
 end
 
--- convert integer to hex
-function num2hex(num)
-   local hexstr = '0123456789abcdef'
-   local s = ''
-   while num > 0 do
-      local mod = math.fmod(num, 16)
-      s = string.sub(hexstr, mod+1, mod+1) .. s
-      num = math.floor(num / 16)
-   end
-   if s == '' then s = '0' end
-   return s
-end
-
 -- simple loop-write from 0x07 to 0xff
 function main()
-	local mycmd=""
-        local startaddr=0
+	local cmd = ''
 	local i
-	core.clearCommandBuffer()
-	for i = 8, 255 do
-            if i <= 15 then 
-		startaddr = "0x0" .. num2hex(i)
-	    else
-		startaddr = "0x" .. num2hex(i)
-	    end
-	    mycmd="hf legic write " .. startaddr .. " 0x01"
-	    print(mycmd)
-	    core.console( mycmd )
+	for i = 7, 255 do
+	    cmd = ('hf legic write 0x%02x 0x01'):format(i)
+	    print(cmd)
+		core.clearCommandBuffer()
+	    core.console(cmd)
 		
 		-- got a 'cmd-buffer overflow' on my mac - so just wait a little
 		-- works without that pause on my linux-box
